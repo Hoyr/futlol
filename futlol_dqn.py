86c8ceef5cada45744db6c6a39b38287aecf5384
Hoyr <dain.harmon@gmail.com>
2020-12-14 14:57:21 -0900

    Update futlol_dqn.py

--- futlol_dqn.py
+++ futlol_dqn.py
@@ -1,4 +1,4 @@
-# Copyright 2019 DeepMind Technologies Ltd. All rights reserved.
+# Dain Harmon 2020/12/14
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""DQN agents trained on Skat by independent Q-learning."""
+"""DQN agents trained on Futlol by independent Q-learning."""
 
 from __future__ import absolute_import
 from __future__ import division
