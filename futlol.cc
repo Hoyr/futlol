7315649473e6d3d15cd96c50dcdfc8906209f23b
Hoyr <dain.harmon@gmail.com>
2020-12-14 14:34:48 -0900

    Update futlol.cc

--- futlol.cc
+++ futlol.cc
@@ -1,4 +1,4 @@
-// Copyright 2019 DeepMind Technologies Ltd. All rights reserved.
+// Dain Harmon 2020/12/14
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
